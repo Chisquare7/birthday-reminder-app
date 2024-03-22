f407bda2e52fca4e43ad1bff25d0afc2d9d238f9
Chisquare7 <chijiokechibuike.f@gmail.com>
2024-03-22 07:24:20 +0100

    Adjusted the cron job time to 7a.m

--- public/index.js
+++ public/index.js
@@ -33,54 +33,12 @@ dob.oninput = function () {
     }
 }
 
-// submit.onclick = function (event) {
-//     event.preventDefault();
 
-//     if (!username.checkValidity() || !email.checkValidity() || !dob.checkValidity()) {
-//         username.reportValidity();
-//         email.reportValidity();
-//         dob.reportValidity();
-//     } else {
-//         fetch("/add-birthday", {
-//             method: "POST",
-//             headers: {
-//                 "Content-Type": "application/json",
-//             },
-//             body: JSON.stringify({ username, email, dob }),
-//         })
-//         .then(response => response.json())
-//         .then(data => {
-//             console.log("success:", data);
-
-//             const successMessage = document.getElementById("successMessage");
-//             successMessage.textContent = "Hurray! Your Birthday successfully added";
-//             successMessage.style.color = "green";
-//             successMessage.style.display = "block";
-
-//             document.getElementById("birthdayForm").reset();
-//         })
-//         .catch((error) => {
-//             console.log("Error:", error);
-
-//             const errorMessage = document.getElementById("errorMessage");
-//             errorMessage.textContent = "Oops! Error adding birthday encountered. Please try again.";
-//             errorMessage.style.color = "red";
-//             errorMessage.style.display = "block"
-//         })
-//     }
-// }
 
 document
   .getElementById("birthdayForm")
   .addEventListener("submit", function (event) {
     event.preventDefault();
-
-    // if (!username.checkValidity() || !email.checkValidity() || !dob.checkValidity()) {
-    //   username.reportValidity();
-    //   email.reportValidity();
-    //   dob.reportValidity();
-    //   return; 
-    // }
 
     const username = document.getElementById("username").value;
     const email = document.getElementById("email").value;
